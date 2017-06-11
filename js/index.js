02b83f75f6de3bea14c152be579957edd4decc9a
Luz Gutierrez <mili01gm@gmail.com>
2017-06-11 16:06:08 -0500

    same functions for mexico

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 $(() => {
-    var i = 0;
+    let i = 0;
     let score = 0;
     let indexImg = 1;
+    const imageP = $('#peru img');
+    const imageM = $('#mexico img');
 
     $("select").on("change", (e) => {
         e.preventDefault();
@@ -11,63 +13,66 @@ $(() => {
             $('#mexico').addClass("none");
             $('#peru').toggleClass("none");
 
-            var peruPhoto = "img/fotos/peru/" + peru[i].image;
-            const image = $('#peru img');
-            image.attr("src", peruPhoto);
-
-            function comparePeru(valor) {
-                if (valor.toLowerCase() != peru[i].name) {
-                    score += 1;
-                    console.log(score);
-                    return score;
-                }
-            }
-
-            function photoChange(valor) {
-                peruPhoto = "img/fotos/peru/" + peru[valor].image;
-                image.attr("src", peruPhoto);
-            }
-
-            $('#peru button').on('click', () => {
-                comparePeru($('input').val());
-                if (score >= 5) {
-                    i += 1;
-                    photoChange(i);
-                }
-            });
+            let peruPhoto = "img/fotos/peru/" + peru[i].image;
+            imageP.attr("src", peruPhoto);
 
         } else {
             $('#peru').addClass("none");
             $('#mexico').toggleClass("none");
+
+            let mexPhoto = "img/fotos/mexico/" + mexico[i].image;
+            imageM.attr("src", mexPhoto);
         }
 
     });
 
+    // Funciones PERU
+    function comparePeru(valor) {
+        if (valor.toLowerCase() != peru[i].name) {
+            score += 1;
+            $('#peru input').text = "";
+            return score;
+        }
+    }
 
-    // for (i = 0; i < peru.lenght;) {
-    //     const photo = peru[i].image;
-    //     image.attr("src", "img/fotos/peru/" + photo);
+    function photoPeru(valor) {
+        peruPhoto = "img/fotos/peru/" + peru[valor].image;
+        $()
+        imageP.attr("src", peruPhoto);
+        score = 0;
+    }
 
-    //     if ($('#coder-name').val().toLowerCase() != peru[i].name.toLowerCase()) {
-    //         trial++;
-    //         if (trial >= 5) {
-    //             i++;
-    //         }
-    //     }
-    // }
+    $('#peru button').on('click', (e) => {
+        e.preventDefault();
+        comparePeru($('input').val());
+        if (score >= 5) {
+            i += 1;
+            photoPeru(i);
+        }
+    });
+
+    //Funciones MEXICO
+    function compareMex(valor) {
+        if (valor.toLowerCase() != mexico[i].name) {
+            score += 1;
+            return score;
+        }
+    }
+
+    function photoMex(valor) {
+        mexPhoto = "img/fotos/mexico/" + mexico[valor].image;
+        $()
+        imageM.attr("src", mexPhoto);
+        score = 0;
+    }
+
+    $('#mexico button').on('click', (e) => {
+        e.preventDefault();
+        compareMex($('input').val());
+        if (score >= 5) {
+            i += 1;
+            photoMex(i);
+        }
+    });
 
 })
-
-
-
-// var slideIndex = 1;
-// showDivs(slideIndex);
-
-// function showDivs(n) {
-//   if (n > peru.length) {slideIndex = 1}    
-//   if (n < 1) {slideIndex = peru.length}
-//   for (i = 0; i < peru.length; i++) {
-//      peru[i].style.display = "none";  
-//   }
-//   peru[slideIndex-1].style.display = "block";  
-// }
