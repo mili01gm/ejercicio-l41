61bdb0a2bbf16e23229e85258d28920152d4f059
Luz Gutierrez <mili01gm@gmail.com>
2017-06-11 20:38:53 -0500

    ongoing succes, fail and missed messages

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ $(() => {
     let indexImg = 1;
     const imageP = $('#peru img');
     const imageM = $('#mexico img');
+    $('#miss').hide();
+    $('#fail').hide();
+    $('#done').hide();
 
     $("select").on("change", (e) => {
         e.preventDefault();
@@ -27,29 +30,39 @@ $(() => {
     });
 
     // Funciones PERU
+    $('#peru button').on('click', (e) => {
+        e.preventDefault();
+        comparePeru($('#peru input').val());
+    });
+
     function comparePeru(valor) {
-        if (valor.toLowerCase() != peru[i].name) {
+        if (valor.toLowerCase() != peru[i].name.toLowerCase()) {
             score += 1;
+            if (score < 5) {
+                $('#peru input').val(" ");
+                $('#miss').show();
+                setTimeout($('#miss').hide(), 2000);
+
+                return score;
+            } else if (score >= 5) {
+                i += 1;
+                return photoPeru(i);
+            }
+
             $('#peru input').text = "";
             return score;
+        } else if (valor.toLowerCase() === peru[i].name.toLowerCase()) {
+            i += 1;
+            score = 0;
+            return photoPeru(i);
         }
     }
 
     function photoPeru(valor) {
         peruPhoto = "img/fotos/peru/" + peru[valor].image;
-        $()
         imageP.attr("src", peruPhoto);
         score = 0;
-    }
-
-    $('#peru button').on('click', (e) => {
-        e.preventDefault();
-        comparePeru($('input').val());
-        if (score >= 5) {
-            i += 1;
-            photoPeru(i);
-        }
-    });
+    };
 
     //Funciones MEXICO
     function compareMex(valor) {
