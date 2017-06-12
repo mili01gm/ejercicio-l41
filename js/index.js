3e2b3f3893f0a165c1de7a1e2ff2fa4669eade20
Luz Gutierrez <mili01gm@gmail.com>
2017-06-12 11:52:55 -0500

    Mexico works. css on work

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 $(() => {
-    let i = 0;
+    let i = Math.floor(Math.random() * peru.length);
+    let j = Math.floor(Math.random() * mexico.length);
     let puntaje = 0;
     let score = 0;
-    let indexImg = 1;
+    let fails = 0;
+    let studentsP = 0;
+    let studentsM = 0;
     const imageP = $('#peru img');
     const imageM = $('#mexico img');
     const inputP = $('#peru input');
@@ -20,20 +23,28 @@ $(() => {
             let nombre = peru[i].name;
             imageP.attr("src", peruPhoto);
             $('#name').text(nombre);
+            puntaje = 0;
         } else {
             $('#peru').addClass("none");
             $('#mexico').toggleClass("none");
 
-            let mexPhoto = "img/fotos/mexico/" + mexico[i].image;
+            let mexPhoto = "img/fotos/mexico/" + mexico[j].image;
+            let nombre = mexico[j].name;
             imageM.attr("src", mexPhoto);
+            $('#mexico-name').text(nombre);
+            puntaje = 0;
         }
     });
 
     // Funciones PERU
     $('#peru button').on('click', (e) => {
         e.preventDefault();
-        inputP.attr("autofocus");
         comparePeru(inputP.val());
+        if (studentsP == peru.lenght - 1 || fails === 3) {
+            $('#peru').toggleClass('none');
+            $('#final').toggleClass('none');
+            $('#final-score').text(puntaje);
+        }
     });
 
     function comparePeru(valor) {
@@ -44,8 +55,10 @@ $(() => {
                 $('#peru .miss').fadeIn(600).delay(1200).fadeOut(600);
                 return score;
             } else if (score >= 5) {
-                i += 1;
+                i = Math.floor(Math.random() * peru.length);
                 puntaje -= 1;
+                fails += 1;
+                studentsP += 1;
                 inputP.val("");
                 $('#peru .fail').fadeIn(600).delay(1200).fadeOut(600);
                 $('#score').text(puntaje);
@@ -55,8 +68,9 @@ $(() => {
             $('#peru input').text = "";
             return score;
         } else if (valor.toLowerCase() === peru[i].name.toLowerCase() && score < 5) {
-            i++;
+            i = Math.floor(Math.random() * peru.length);
             puntaje += 5;
+            studentsP += 1;
             score = 0;
             inputP.val("");
             $('#peru .done').fadeIn(600).delay(1200).fadeOut(600);
@@ -68,34 +82,59 @@ $(() => {
     function photoPeru(i) {
         peruPhoto = "img/fotos/peru/" + peru[i].image;
         nombre = peru[i].name;
-        imageP.attr("src", peruPhoto);
+        imageP.fadeIn(2000).attr("src", peruPhoto);
         $('#name').text(nombre);
         inputP.attr("autofocus", "");
         score = 0;
     };
 
-    //Funciones MEXICO
-    function compareMex(valor) {
-        if (valor.toLowerCase() != mexico[i].name) {
-            score += 1;
-            return score;
-        }
-    }
-
-    function photoMex(valor) {
-        mexPhoto = "img/fotos/mexico/" + mexico[valor].image;
-        $()
-        imageM.attr("src", mexPhoto);
-        score = 0;
-    }
-
+    // Funciones MEXICO
     $('#mexico button').on('click', (e) => {
         e.preventDefault();
-        compareMex($('input').val());
-        if (score >= 5) {
-            i += 1;
-            photoMex(i);
+        compareMex(inputM.val());
+        if (studentsM == mexico.lenght - 1 || fails === 3) {
+            $('#mexico').toggleClass('none');
+            $('#final').toggleClass('none');
+            $('#final-score').text(puntaje);
         }
     });
 
+    function compareMex(valor) {
+        if (valor.toLowerCase() != $('#name').text().toLowerCase()) {
+            score++;
+            if (score < 5) {
+                inputM.val("");
+                $('#mexico .miss').fadeIn(600).delay(1200).fadeOut(600);
+                return score;
+            } else if (score >= 5) {
+                j = Math.floor(Math.random() * mexico.length);
+                puntaje -= 1;
+                fails += 1;
+                studentsM += 1;
+                inputM.val("");
+                $('#mexico .fail').fadeIn(600).delay(1200).fadeOut(600);
+                $('#score').text(puntaje);
+                return photoMex(j);
+            }
+            return score;
+        } else if (valor.toLowerCase() === mexico[j].name.toLowerCase() && score < 5) {
+            j = Math.floor(Math.random() * mexico.length);
+            puntaje += 5;
+            studentsM += 1;
+            score = 0;
+            inputM.val("");
+            $('#mexico .done').fadeIn(600).delay(1200).fadeOut(600);
+            $('#score').text(puntaje);
+            return photoMex(j);
+        }
+    }
+
+    function photoMex(j) {
+        mexPhoto = "img/fotos/mexico/" + mexico[j].image;
+        nombre = mexico[j].name;
+        imageM.fadeIn(2000).attr("src", mexPhoto);
+        $('#mexico-name').text(nombre);
+        inputM.attr("autofocus", "");
+        score = 0;
+    };
 })
